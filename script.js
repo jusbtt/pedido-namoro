07d141fb71a979f0365f7417e0920612d0021f98
Julyane Silva <117245739+jusbtt@users.noreply.github.com>
2024-02-06 22:58:49 -0300

    Subi os arq

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById("sim").addEventListener("click", function() {
+    window.location.href = "https://www.rosealiancas.com.br/produto-alianca-de-compromisso-3mm-prata-950-modelo-infinity-p0072?gad_source=1&gclid=Cj0KCQiAzoeuBhDqARIsAMdH14FpmAxsP3Cke2_4j8qVcr3wx8b3ZVkvMjBxnkHmw7VIiecmjr8HXh0aAgr0EALw_wcB";
+  });
+  
+  document.getElementById("nao").addEventListener("click", function() {
+    window.location.href = "https://www.myinstants.com/pt/instant/voce-ta-fudido-quando-eu-lhe-pegar-73430/";
+  });
+  
